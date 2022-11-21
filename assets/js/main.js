b0a3557fa8fe419b37575092fd6fb6a99fc5bd95
Gustavo edson <ggugu9289@gmail.com>
2022-11-21 00:17:07 -0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// const content = document.querySelector(".content");
+
+// content.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   const el = e.target;
+//   if (el.classList.contains("submit")) {
+//     content.st
+//   }
+// });
+
+// after
